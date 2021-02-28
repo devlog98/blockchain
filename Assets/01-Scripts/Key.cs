ff78f360e95e9fde2d8e21433bcdf3dc9941c6d8
devlog98 <devlog98@outlook.com>
2021-02-27 21:43:21 -0300

    [ADD] LockBlock and Key

---
+++ Assets/01-Scripts/Key.cs
@@ -0,0 +1,48 @@
+﻿using devlog98.Block;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Key composition that unlocks specific Locks
+ * Dependent on Player Blocks composition
+ */
+
+namespace devlog98.Level {
+    public class Key : MonoBehaviour {
+        private List<PlayerBlock> keyBlocks = new List<PlayerBlock>();
+
+        private void Start() {
+            keyBlocks.AddRange(GetComponentsInChildren<PlayerBlock>());
+            foreach (PlayerBlock keyBlock in keyBlocks) {
+                keyBlock.CheckBlockNeighbours();
+            }
+        }
+
+        // check Player Blocks composition for specific Lock
+        public bool CheckAgainstKeyBlocks(List<PlayerBlock> blocks) {
+            bool check = true;
+
+            foreach(PlayerBlock keyBlock in keyBlocks) {
+                // find block with same properties
+                PlayerBlock block = blocks.Find(x =>
+                    (x.RightBlock == null) == (keyBlock.RightBlock == null) &&
+                    (x.LeftBlock == null) == (keyBlock.LeftBlock == null) &&
+                    (x.UpBlock == null) == (keyBlock.UpBlock == null) &&
+                    (x.DownBlock == null) == (keyBlock.DownBlock == null)
+                );
+
+                // remove block from original list
+                if (block != null) {                    
+                    blocks.Remove(block);
+                }
+            }
+
+            // if there are blocks left, Player composition is wrong
+            if (blocks.Count > 0) {
+                check = false;
+            }
+
+            return check;
+        }
+    }
+}
---
+++ Assets/01-Scripts/LockBlock.cs
@@ -0,0 +1,21 @@
+﻿using devlog98.Actor;
+using devlog98.Level;
+using UnityEngine;
+
+namespace devlog98.Block {
+    public class LockBlock : MonoBehaviour {
+        [Header("Lock")]
+        [SerializeField] private Key key;
+
+        private void OnTriggerEnter2D(Collider2D collision) {
+            if (collision.tag == "Block") {
+                if (key.CheckAgainstKeyBlocks(Player.instance.Blocks)) {
+                    Debug.Log("Desbloqueou a fase!!!");
+                }
+                else {
+                    Debug.Log("Errou a composição!!!");
+                }
+            }
+        }
+    }
+}
