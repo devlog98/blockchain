62748cd75c8e136eade0dcec127d3cae9a6d8976
devlog98 <devlog98@outlook.com>
2021-03-01 05:28:58 -0300

    [MOD] Final adjustments

--- Assets/01-Scripts/AudioManager.cs
+++ Assets/01-Scripts/AudioManager.cs
@@ -17,7 +17,6 @@ namespace devlog98.Audio {
         }
 
         public void PlayOneShot(AudioClip clip) {
-            Debug.Log("Tocou aqui!");
             source.PlayOneShot(clip);
         }
     }
