a61358e015ddcf75d34eb1d6348d289945802290
devlog98 <devlog98@outlook.com>
2021-03-30 14:06:09 -0300

    [ADD] Show objects based on build platform

---
+++ Assets/01-Scripts/ShowOnPlatform.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/*
+ * Shows or hides game objects based on specific device of the game
+ */
+
+namespace devlog98.Platforms {
+    public enum PlatformType { Standalone, Mobile}
+
+    public class ShowOnPlatform : MonoBehaviour {
+        [SerializeField] private PlatformType platform;
+
+        private void Start() {
+            #if UNITY_STANDALONE
+                if (platform == PlatformType.Standalone) {
+                    this.gameObject.SetActive(true);
+                }
+                else {
+                    this.gameObject.SetActive(false);
+                }
+            #endif
+
+            #if UNITY_ANDROID
+                if (platform == PlatformType.Mobile) {
+                    this.gameObject.SetActive(true);
+                }
+                else {
+                    this.gameObject.SetActive(false);
+                }
+            #endif
+        }
+    }
+}
