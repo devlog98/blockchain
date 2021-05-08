bbd083d8c2aa09dc8bc2178e427911204c55aa6a
devlog98 <devlog98@outlook.com>
2021-05-08 10:38:55 -0300

    [ADD] Post Process optimization on slower devices

---
+++ Assets/01-Scripts/PostProcessManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+/*
+ * Activates post process based on memory budget
+ */
+
+namespace devlog98.Platforms {
+    public class PostProcessManager : MonoBehaviour {
+        [SerializeField] private PostProcessVolume volume;
+
+        private void Awake() {
+            // At least 2GB of total memory or 1GB of VRAM
+            if (SystemInfo.graphicsMemorySize >= 1024) {
+                volume.enabled = true;
+            }
+        }
+    }
+}
